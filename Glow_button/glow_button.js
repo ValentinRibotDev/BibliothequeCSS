67f93e991f8c51ddca792e6999e6eda836d31c17
ValentinRibot <valentinribot91@gmail.com>
2025-07-15 15:53:27 +0200

    glow_glass_button ajouté

--- Glow_button/glow_button.js
+++ Glow_button/glow_button.js
@@ -1,11 +1,14 @@
-let btn = document.querySelector('.glow-btn'); // Recuperation du bouton 
+let glow_btns = document.querySelectorAll('.glow-btn'); // Recuperation de tous les boutons avec la class 'glow-btn'
 
-//event lors du déplacement de la sourie
-btn.onmousemove = function(e){
-    let x = e.pageX - btn.offsetLeft; //position x de la sourie sur la page
-    let y = e.pageY - btn.offsetTop; // poistion y de la sourie sur la page
+//Boucle foreach
+glow_btns.forEach(btn => {
+    //event lors du déplacement de la souris
+    btn.onmousemove = function(e){
+        let x = e.pageX - btn.offsetLeft; //position x de la souris sur la page
+        let y = e.pageY - btn.offsetTop; // poistion y de la souris sur la page
 
-    //ajouts des propriétés défini précedement 
-    btn.style.setProperty('--x', x+'px');
-    btn.style.setProperty('--y', y+'px');
-}
+        //ajouts des propriétés défini précedement 
+        btn.style.setProperty('--x', x+'px');
+        btn.style.setProperty('--y', y+'px');
+    }
+})
